1f20a0f2470f06ad45448a6e531ef03b27c27ed6
CyberChicka <gil5656565656@gmail.com>
2023-06-16 16:27:46 +0300

    new class CreateText

--- Code/Config.h
+++ Code/Config.h
@@ -11,6 +11,7 @@ using namespace sf;
 using namespace std;
 
 #include "Image/CreateImage.h"
+#include "Text/CreateText.h"
 #include "Enemy/Enemy.h"
 
 #include "Melody/m_Music.h"
@@ -34,6 +35,8 @@ public:
     CreateImage *s_player;
     CreateImage *s_EnemyGhost, *s_EnemySkeleton;
     CreateImage *s_LvL1, *s_LvL2, *s_LvL3;
+    // Text
+    CreateText *text_nps_history;
     // Music & sound
     m_Music *m_lvl1;
     m_Music *m_lvl2;
@@ -41,7 +44,9 @@ public:
     //player
     s_Sound *so_player_attack;
     //enemy
-    s_Sound *so_enemy_attack;
+    s_Sound *so_enemy_Ghost_attack;
+    s_Sound *so_enemy_Scelet_attack;
+    //
     Config();
     static Config *instance;
 private:
---
+++ Code/Text/CreateText.h
@@ -0,0 +1,27 @@
+//
+// Created by Doni on 16.06.2023.
+//
+
+#ifndef GAMEENGINE_BROUGHT_CREATETEXT_H
+#define GAMEENGINE_BROUGHT_CREATETEXT_H
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+class CreateText {
+public:
+    int size_text;
+    string name_text;
+    string file;
+    Text *text;
+    Font *font;
+    CreateText(string File, int Size_Text, string Name_text);
+    CreateText operator+(const CreateText &other);
+    ~CreateText();
+    void setString(string Name_Text);
+};
+
+
+#endif
